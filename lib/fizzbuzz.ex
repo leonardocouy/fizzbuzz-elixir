697d618194f4bb52e5ecb7091780c479df479cb3
Leonardo Flores <contato@leonardocouy.com>
2022-04-13 08:50:11 -0300

    implement fizzbuzz logic

--- lib/fizzbuzz.ex
+++ lib/fizzbuzz.ex
@@ -2,9 +2,29 @@ defmodule Fizzbuzz do
   def build(filename) do
     filename
     |> File.read()
-    |> handle_file()
+    |> handle_file(filename)
   end
 
-  def handle_file({:ok, content}), do: String.trim(content)
-  def handle_file({:error, reason}), do: reason
+  defp handle_file({:ok, content}, _filename) do
+    content =
+      content
+      |> String.trim
+      |> String.split(",")
+      |> Enum.map(&convert_and_evaluate_numbers/1)
+
+    {:ok, content}
+  end
+
+  defp handle_file({:error, reason}, filename), do: "Error reading the file '#{filename}' #{reason}"
+
+  defp convert_and_evaluate_numbers(elem) do
+    elem
+    |> String.to_integer
+    |> evaluate_Number
+  end
+
+  defp evaluate_Number(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
+  defp evaluate_Number(number) when rem(number, 3) == 0, do: :fizz
+  defp evaluate_Number(number) when rem(number, 5) == 0, do: :buzz
+  defp evaluate_Number(number), do: number
 end
